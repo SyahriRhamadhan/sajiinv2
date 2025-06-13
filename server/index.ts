22930dcb44f893af0b2631a733a2a5ebf2eb68fd
SyahriRhamadhan <2001020038@student.umrah.ac.id>
2025-06-13 10:41:16 +0700

    chore: audit security, remove old db connection, optimize server config

--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,20 @@ import express from "express";
 import type { Express } from "express";
 import * as path from "path";
 import { createRequestHandler } from "@remix-run/express";
+import helmet from "helmet";
 
 const app = express();
 
+// Tambahkan Helmet untuk keamanan dasar HTTP headers
+app.use(helmet());
+
 // Serve static files
 app.use(express.static(path.join(__dirname, "../public")));
+
+// Simple API endpoint for health check
+app.get("/api/cek", (req, res) => {
+  res.json({ status: "ok", message: "API is working!" });
+});
 
 // Remix request handler
 app.all(
